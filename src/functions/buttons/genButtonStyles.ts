01ff2760c521c349bc380c0d81b3ab367c10b14b
LeonardoSouzaBento <bentosouzaleonardo@gmail.com>
2026-01-19 08:30:48 -0300

    docs: adição de estilos globais na saída dos estilos do botão.

--- src/functions/buttons/genButtonStyles.ts
+++ src/functions/buttons/genButtonStyles.ts
@@ -47,7 +47,7 @@ export const genButtonStyles = (
 
   const fillPadding = getPadding(fillPaddings);
   const outlinePadding = getPadding(outlinePaddings);
-  const buttonStyles = `default:\n'bg-primary hover:bg-primary/90 text-primary-50 disabled:bg-neutral-300 disabled:text-neutral-500/80',\n
+  const buttonStyles = `globais:\nh-fit flex items-center box-border leading-none!;\n\ndefault:\n'bg-primary hover:bg-primary/90 text-primary-50 disabled:bg-neutral-300 disabled:text-neutral-500/80',\n
 outline:\n'border-${outlineValue} border-primary text-primary bg-transparent hover:bg-primary-50/50 disabled:bg-neutral-100 disabled:border-neutral-300 disabled:text-neutral-500/75',\n
 ghost:\n'hover:bg-primary-50 border text-primary bg-transparent disabled:bg-neutral-100 disabled:text-neutral-400 disabled:border-none',\n
 secondary:\n'bg-primary-100 text-primary hover:bg-primary-200 disabled:bg-neutral-100 disabled:text-neutral-400 disabled:border-none',...};\n`;
@@ -56,9 +56,9 @@ secondary:\n'bg-primary-100 text-primary hover:bg-primary-200 disabled:bg-neutra
 sm:\n '${fillPadding[0]} text-sm-button',\n
 default:\n '${fillPadding[1]} text-button',\n
 lg:\n '${fillPadding[2]} text-lg-button',\n
-outline-sm:\n '${outlinePadding[0]} text-sm-button',\n
-outline-default:\n '${outlinePadding[1]} text-button',\n
-outline-lg:\n '${outlinePadding[2]} text-lg-button',\n
+"outline-sm":\n '${outlinePadding[0]} text-sm-button',\n
+"outline-default":\n '${outlinePadding[1]} text-button',\n
+"outline-lg":\n '${outlinePadding[2]} text-lg-button',\n
 ${iconSizes}
 }`;
 
